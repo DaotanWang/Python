38de184cdaeaf8af9f8e2d37877c85ef1646b6aa
Douglas Wang <132033521+DaotanWang@users.noreply.github.com>
2023-06-21 11:42:00 +0800

    Update Excel per row to a list.py

--- Excel processing /Excel per row to a list.py
+++ Excel processing /Excel per row to a list.py
@@ -1,22 +1,24 @@
 df1 = pd.read_excel(r'C:\excel.xlsx', skiprows=1 , header=None)
+#df1 = pd.read_excel(r'C:\keywords.xlsx', sheet_name='Sheet1', usecols=[1],names=None)
 df1_list = df1.values.tolist()
 
-- io - 文件路径或file-like对象。指定Excel文件读取的来源。
-- sheet_name - 工作表名,读取指定工作表。默认读入所有工作表。
-- sheet_names - 工作表列表,读取指定工作表列表。
-- header - 行数或行序号,指定列头所在行。默认为0,使用第一行作为列头。
-- skiprows - 要跳过的行数或行号列表,跳过文件中的指定行。
-- index_col - 行序号或行号列表,将指定行用作行索引。
-- names - 列名列表,指定 DataFrame 的列名。
-- na_values - 一组待替换的 NaN 值。
-- parse_dates - 要作为日期列解析的列序号或列名列表。
-- date_parser - 用于解析日期值的函数。
-- nrows - 读取前 nrows 行。
-- dtype - 数据类型,强制转换数据至指定类型。
-- engine - 文件引擎之间进行切换 {'xlrd', 'openpyxl', 'odf'}。
-- converters - 列与转换器之间的映射,用于在解析之前转换数据。
-- true_values - 要在布尔型列中视为 true 的字符串列表。
-- false_values - 要在布尔型列中视为 false 的字符串列表。
-- squeeze - 如果工作表只有一列,则返回 Series。
-- comment - 用于记录记录行号以略过的字符。
-- thousands - 用于分组的千位分隔符。
+# - usecols - 只加载这一列，也能接列名。
+# - io - 文件路径或file-like对象。指定Excel文件读取的来源。
+# - sheet_name - 工作表名,读取指定工作表。默认读入所有工作表。
+# - sheet_names - 工作表列表,读取指定工作表列表。
+# - header - 行数或行序号,指定列头所在行。默认为0,使用第一行作为列头。
+# - skiprows - 要跳过的行数或行号列表,跳过文件中的指定行。
+# - index_col - 行序号或行号列表,将指定行用作行索引。
+# - names - 列名列表,指定 DataFrame 的列名。
+# - na_values - 一组待替换的 NaN 值。
+# - parse_dates - 要作为日期列解析的列序号或列名列表。
+# - date_parser - 用于解析日期值的函数。
+# - nrows - 读取前 nrows 行。
+# - dtype - 数据类型,强制转换数据至指定类型。
+# - engine - 文件引擎之间进行切换 {'xlrd', 'openpyxl', 'odf'}。
+# - converters - 列与转换器之间的映射,用于在解析之前转换数据。
+# - true_values - 要在布尔型列中视为 true 的字符串列表。
+# - false_values - 要在布尔型列中视为 false 的字符串列表。
+# - squeeze - 如果工作表只有一列,则返回 Series。
+# - comment - 用于记录记录行号以略过的字符。
+# - thousands - 用于分组的千位分隔符。
